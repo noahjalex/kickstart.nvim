623d4af59c36ce6eb2ffa8c46a7f0ba104d6e0ee
Noah <nalexander@squareup.com>
2025-12-02 19:58:10 -0500

    switch to unnamed plus

--- init.lua
+++ init.lua
@@ -114,9 +114,7 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
-end)
+vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
