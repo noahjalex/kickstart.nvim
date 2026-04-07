9ee56021cfe9ae15ae58896b34bb95e3caf7129b
Noah <noahjalex@gmail.com>
2026-04-07 11:04:02 -0400

    set ts formatter to 2-space; don't auto-format on save

--- init.lua
+++ init.lua
@@ -800,7 +800,7 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { c = true, cpp = true, typescript = true, typescriptreact = true, javascript = true, javascriptreact = true }
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
 				else
@@ -816,7 +816,18 @@ require("lazy").setup({
 				-- python = { "isort", "black" },
 				--
 				-- You can use 'stop_after_first' to run the first available formatter from the list
-				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+				typescript = { "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				javascript = { "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			},
+			formatters = {
+				prettier = {
+					prepend_args = { "--tab-width", "2" },
+				},
+				prettierd = {
+					prepend_args = { "--tab-width", "2" },
+				},
 			},
 		},
 	},
