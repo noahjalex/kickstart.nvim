c83347fdfdd4b2aed532f11ee1bd26f9c5753b1d
Noah <noahjalex@gmail.com>
2026-05-25 14:10:37 -0400

    gitsigns new cmd <leader>gi for inline previews

--- init.lua
+++ init.lua
@@ -289,6 +289,17 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gitsigns = require 'gitsigns'
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map('n', '<leader>gi', gitsigns.preview_hunk_inline)
+      end,
     },
   },
 
